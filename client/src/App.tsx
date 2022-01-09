42c991d2f163538baf19ad3420223084166b6760
Rupinder Hayer <ruphayer@gmail.com>
2022-01-08 23:37:41 -0800

    added more folders/components to build the basic structure for the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Form from './components/form/Form';
+import Results from './components/results/Results';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Home/>
+      <Form/>
+      <Results/>
+      <SingleResult/>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/form/Form.tsx
@@ -0,0 +1,9 @@
+export default function Form (){
+  return(
+    <form>
+    <label >Enter movie / tv shows</label>
+    <input type="text" />
+    <input type="submit" value="Submit"/>
+  </form> 
+  )
+}
