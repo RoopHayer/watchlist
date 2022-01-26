8909d90fba81a434f10169269ad6fbbc6e385877
Rupinder Hayer <ruphayer@gmail.com>
2022-01-25 23:55:50 -0800

    added savecontent, created profile component to render saved content for user

--- client/src/components/profileSingleContent/ProfileSingleContent.tsx
+++ client/src/components/profileSingleContent/ProfileSingleContent.tsx
@@ -1,15 +1,15 @@
 import { Card } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { saveContentToProfile } from "../../store/saveContentSlice";
-export default function SingleResult(props: any) {
+// import { useDispatch } from "react-redux";
+// import { saveContentToProfile } from "../../store/saveContentSlice";
+export default function ProfileSingleContent(props: any) {
   const singleContent: any = props.content;
-  const dispatch = useDispatch();
-  const handleClick = (e: any) => {
-    dispatch(saveContentToProfile(singleContent));
-  };
+  // const dispatch = useDispatch();
+  // const handleClick = (e: any) => {
+  //   dispatch(saveContentToProfile(singleContent));
+  // };
   return (
     <>
-      <Card onClick={handleClick} className="content-card">
+      <Card className="content-card">
         <Card.Img
           variant="top"
           src={singleContent.Poster}
