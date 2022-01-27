a27209f5a7f267da8763d3cbab8def85553d7607
Rupinder Hayer <ruphayer@gmail.com>
2022-01-27 00:52:10 -0800

    added components in home component to organize layout better, add search bar on navbar

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,25 +1,33 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Form } from "react-bootstrap";
+
 export default function Header() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" style={{ width: "100%" }}>
-        <Container>
-          <Navbar.Brand href="#home">Watchlist</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to={"/"}>
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/profile"}>
-              Profile
-            </Nav.Link>
-          </Nav>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Signed in as: <a href="#login">Roop Hayer</a>
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        style={{ width: "100%", paddingLeft: " 10rem", paddingRight: " 10rem" }}
+      >
+        <Navbar.Brand href="#home">Watchlist</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to={"/"}>
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to={"/profile"}>
+            Profile
+          </Nav.Link>
+        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Form style={{ marginRight: "2rem" }}>
+            <Form.Control type="text" placeholder="Enter Movies/TV Shows" />
+          </Form>
+          <Navbar.Text>
+            Signed in as: <a href="#login">Roop Hayer</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
       </Navbar>
       <br />
     </>
