bad1a31de6a99c41dfb1e7b12a65fccb5547f06d
Rupinder Hayer <ruphayer@gmail.com>
2022-02-01 23:54:32 -0800

    added more styling for profile page, added form and 2 buttons

--- client/src/components/profileSingleContent/ProfileSingleContent.tsx
+++ client/src/components/profileSingleContent/ProfileSingleContent.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Form, Button } from "react-bootstrap";
 export default function ProfileSingleContent(props: any) {
   const singleContent: any = props.content;
 
@@ -10,9 +10,21 @@ export default function ProfileSingleContent(props: any) {
           src={singleContent.Poster}
           style={{ width: "100%", height: "28rem" }}
         />
-        <Card.Body style={{ height: "auto", width: "auto" }}>
+        <Card.Body>
           <Card.Text>{singleContent.Title}</Card.Text>
           <Card.Text>{singleContent.Year}</Card.Text>
+          <Form>
+            <Form.Group className="mb-3" controlId="notes">
+              <Form.Label>Notes</Form.Label>
+              <Form.Control type="title" placeholder="type in your notes" />
+            </Form.Group>
+            <Button variant="warning" type="submit">
+              Add notes
+            </Button>
+          </Form>
+          <Button style={{ margin: "1rem 0" }} variant="warning">
+            Delete
+          </Button>
         </Card.Body>
       </Card>
     </>
